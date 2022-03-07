a090d8e1d1bd0e4d31c0f8faeba81ea2e0440528
alexkotovwa <alex_moscow@inbox.ru>
2022-03-06 22:06:48 -0800

    C# version of the main script

---
+++ main_polaris.cs
@@ -0,0 +1,17 @@
+﻿using Renci.SshNet;
+
+SshClient cSSH = new SshClient("192.168.8.136", 22, "user", "1");
+cSSH.Connect();
+SshCommand x = cSSH.RunCommand("mkdir polaris_showcase");
+ScpClient client = new ScpClient("192.168.8.136", "user", "1");
+client.Connect();
+client.Upload(new FileInfo("D:\\showcase\\remote_script.py"), "/home/user/polaris_showcase_cs/remote_script.py");
+
+SshCommand y = cSSH.RunCommand("python3 /home/user/polaris_showcase_cs/remote_script.py");
+Thread.Sleep(1000);
+client.Download("/home/user/polaris_showcase_cs/report.json", new FileInfo("D:\\showcase\\report.json"));
+
+client.Disconnect();
+client.Dispose();
+cSSH.Disconnect();
+cSSH.Dispose();
